7852c055e13c781199c012f63de056b19eafd723
Andrey.Metelyov <andrey.metelyov@gmail.com>
2021-08-02 14:10:52 +0900

    2/4

--- Number Base Converter/task/src/converter/Main.kt
+++ Number Base Converter/task/src/converter/Main.kt
@@ -1,6 +1,42 @@
 package converter
 
+import kotlin.system.exitProcess
+
+const val digits = "0123456789ABCDEF"
+
 fun main() {
+    while (true) {
+        println("Do you want to convert /from decimal or /to decimal? (To quit type /exit)")
+        val command = readLine()!!
+        when (command) {
+            "/from" -> from()
+            "/to" -> to()
+            "/exit" -> exitProcess(0)
+            else -> println("error")
+        }
+    }
+}
+
+fun to() {
+    println("Enter source number:")
+    val number = readLine()!!
+    println("Enter source base:")
+    val base = readLine()!!.toInt()
+    println("Conversion to decimal result: ${convertTo10(number, base)}")
+}
+
+fun convertTo10(number: String, base: Int): Int {
+    var result = 0
+    var pow = 1
+    for (i in number.lastIndex downTo 0) {
+        System.err.println("${number[i]}(${digits.indexOf(number[i])}) * $pow")
+        result += digits.indexOf(number[i].uppercaseChar()) * pow
+        pow *= base
+    }
+    return result
+}
+
+fun from() {
     println("Enter number in decimal system:")
     val number = readLine()!!.toInt()
     println("Enter target base:")
@@ -9,7 +45,6 @@ fun main() {
 }
 
 fun convert(number: Int, targetBase: Int): String {
-    val digits = "0123456789ABCDEF"
     var num = number
     var result = ""
     while (num >= targetBase) {
